750465612c056aef5c6609720428c6dfed1ae8e7
Keven Andrade <nivekandrade@live.com>
2023-05-16 19:25:04 -0300

    criando model cliente

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(max_length=50, blank=False, unique=True)
+    cpf = models.CharField(max_length=11, unique=True, blank=False)
+    rg = models.CharField(max_length=9, unique=True, blank=False)
+    celular = models.CharField(max_length=14, blank=False)
+    ativo = models.BooleanField()
+    
+    def __str__(self):
+        return self.nome
